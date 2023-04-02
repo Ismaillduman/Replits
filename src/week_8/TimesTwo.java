d1c04081905c16f6dbad3c29a13889a42586a0db
Ismailduman <issmailduman@gmail.com>
2023-04-03 00:46:05 +0200

    multiple two

--- src/week_8/TimesTwo.java
+++ src/week_8/TimesTwo.java
@@ -1,2 +1,27 @@
-package week_8;public class TimesTwo {
+package week_8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class TimesTwo {
+    public static ArrayList<Integer> timesTwo(ArrayList<Integer> nums){
+        // TODO: finish the method
+        ArrayList<Integer> twoTimes= new ArrayList<>();
+        for (int eachNum : nums) {
+            eachNum*=2;
+           twoTimes.add(eachNum);
+        }
+
+
+       // System.out.println(twoTimes);
+
+
+
+return twoTimes;
+    }
+    public static void main(String[] args) {
+ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(1,2,3,4,0));
+        System.out.println(timesTwo(nums));
+    }
 }
