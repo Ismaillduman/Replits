6a24eac15a226b4c03595c06b859d0f306d2d57f
Ismailduman <issmailduman@gmail.com>
2023-04-13 22:33:31 +0200

    diagonal difference

--- src/eu7Arrays/DiagonalDifference.java
+++ src/eu7Arrays/DiagonalDifference.java
@@ -5,23 +5,40 @@ import java.util.Arrays;
 
 public class DiagonalDifference {
     public static void main(String[] args) {
-        int sum = 0;
+        int sumLeftToRight = 0;
+        int sumRightToLeft = 0;
         int[][] nums = {{1, 2, 3}, {4, 5, 6}, {9, 8, 9}};
         for (int i = 0; i < nums.length; i++) {
             for (int k = 0; k < nums.length; k++) {
                 if (i == 0 && k == 0) {
 
-                    sum += nums[i][k];
+                    sumLeftToRight += nums[i][k];
                 }
                 if (i == 1 && k==1) {
-                    sum+=nums[i][k];
+                    sumLeftToRight+=nums[i][k];
                 }
                 if(i==2&&k==2){
-                    sum+=nums[i][k];
+                    sumLeftToRight+=nums[i][k];
                 }
             }
         }
-        System.out.println(sum);
+
+        for (int j = 0; j < nums.length; j++) {
+            for (int k = 0; k < nums.length; k++) {
+                if (j == 0 && k == 2) {
+
+                    sumRightToLeft += nums[j][k];
+                }
+                if (j == 1 && k==1) {
+                    sumRightToLeft+=nums[j][k];
+                }
+                if(j==2&&k==0){
+                    sumRightToLeft+=nums[j][k];
+                }
+            }
+        }
+        int diagonalDifference= sumRightToLeft-sumLeftToRight;
+        System.out.println(Math.abs(diagonalDifference));
         /*
 1 2 3
 4 5 6
